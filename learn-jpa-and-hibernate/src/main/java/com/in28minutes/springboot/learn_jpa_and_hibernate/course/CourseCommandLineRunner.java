121d115251b7bea6f57a327360b4adab9f5689d8
samuelcsng <samuelcsng@gmail.com>
2024-12-19 20:25:11 +0800

    Initial commit

---
+++ learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learn_jpa_and_hibernate/course/CourseCommandLineRunner.java
@@ -0,0 +1,42 @@
+package com.in28minutes.springboot.learn_jpa_and_hibernate.course;
+
+import com.in28minutes.springboot.learn_jpa_and_hibernate.course.jpa.CourseJpaRepository;
+import com.in28minutes.springboot.learn_jpa_and_hibernate.course.springdatajpa.CourseSpringDataJpaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CourseCommandLineRunner implements CommandLineRunner {
+
+//    @Autowired
+//    private CourseJdbcRepository repository;
+
+//    @Autowired
+//    private CourseJpaRepository repository;
+
+    @Autowired
+    private CourseSpringDataJpaRepository repository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        repository.save(new Course(1, "Learn AWS Jpa!", "in28minutes"));
+        repository.save(new Course(2, "Learn Azure Jpa!", "in28minutes"));
+        repository.save(new Course(3, "Learn DevOps Jpa!", "in28minutes"));
+
+        repository.deleteById(1L);
+
+        System.out.println(repository.findById(2L));
+        System.out.println(repository.findById(3L));
+
+        System.out.println(repository.findAll());
+        System.out.println(repository.count());
+
+        System.out.println(repository.findByAuthor("in28minutes"));
+        System.out.println(repository.findByAuthor(""));
+
+        System.out.println(repository.findByName("Learn AWS Jpa!"));
+        System.out.println(repository.findByName("Learn DevOps Jpa!"));
+
+    }
+}
---
+++ myfirstwebapp/README.md
@@ -0,0 +1,29 @@
+- @RequestParam
+- View, jsp, model, ModelMap
+- logging.level, Logger, LoggerFactory
+- Model 1, Model 2
+- MVC, Front Controller, Servlet, Dispatcher Servlet
+- Request vs Model vs Session(@SessionAttributes())
+  - Request Scope vs Session Scope
+- JSTL
+
+- org.webjars.bootstrap
+- org.webjars.jquery
+
+- validation
+  - Spring Boot Starter Validation
+  - Command Bean(Form Backing Object)
+  - spring form tag library
+
+- bootstrap-datepicker
+
+- JSP fragments(header.jspf footer.jspf files)
+
+- Spring Security
+- Spring Boot Starter Security
+  - InMemoryUserDetailsManager, PasswordEncoder
+  - Authentication, SecurityContextHolder
+  - SecurityFilterChain, HttpSecurity
+
+- @Entity, @Id, @GeneratedValue, @Column
+- JpaRepository
